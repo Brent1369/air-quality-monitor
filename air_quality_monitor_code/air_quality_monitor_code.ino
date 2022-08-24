66bd19299dd37420f4295e8f765adbf6e223e73e
Brent Vreys <12000698@student.pxl.be>
2022-08-24 23:20:56 +0200

    Update air_quality_monitor_code.ino

--- air_quality_monitor_code/air_quality_monitor_code.ino
+++ air_quality_monitor_code/air_quality_monitor_code.ino
@@ -1,137 +1,106 @@
-// SoftwareSerial.ino: Read PMS5003 sensor on SWSerial
 
-#ifndef ESP32
+#include <Arduino.h>
+#include "s8_uart.h"
 #include <SoftwareSerial.h>
-#endif
-#include <PMserial.h> // Arduino library for PM sensors with serial interface
+//#include "PMS.h"
+/* BEGIN CONFIGURATION */
+#define DEBUG_BAUDRATE 9600
+#include <PMserial.h>
 
-#if !defined(PMS_RX) && !defined(PMS_TX)
-constexpr auto PMS_RX = 0;
-constexpr auto PMS_TX = 2;
-#endif
+#define S8_RX_PIN 22        // Rx pin which the S8 Tx pin is attached to (change if it is needed)
+#define S8_TX_PIN 23         // Tx pin which the S8 Rx pin is attached to (change if it is needed)
+SoftwareSerial S8_serial(S8_RX_PIN, S8_TX_PIN);
+S8_UART *sensor_S8;
+S8_sensor sensor;
 
-#ifndef ESP32
-SerialPM pms(PMS5003, PMS_RX, PMS_TX); // PMSx003, RX, TX
+#define pms5003_RX_PIN 0        // Rx pin which the S8 Tx pin is attached to (change if it is needed)
+#define pms5003_TX_PIN 2         // Tx pin which the S8 Rx pin is attached to (change if it is needed)
+SerialPM  pms5003_serial(PMS5003, pms5003_RX_PIN, pms5003_TX_PIN);
+//PMS pms(pms5003_serial);
+//PMS::DATA data;
 
-// Alternative:
-//SoftwareSerial SoftSerial1(PMS_RX, PMS_TX);
-//SerialPM pms(PMS5003, SoftSerial1);
-#else
-SerialPM pms(PMS5003, PMS_RX, PMS_TX); // PMSx003, RX, TX
-#endif
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println(F("Booted"));
+void setup() {
+  
+  // Configure serial port, we need it for debug
+  Serial.begin(DEBUG_BAUDRATE);
 
-  Serial.println(F("PMS sensor on SWSerial"));
-  Serial.print(F("  RX:"));
-  Serial.println(PMS_RX);
-  Serial.print(F("  TX:"));
-  Serial.println(PMS_TX);
+  // Wait port is open or timeout
+  int i = 0;
+  while (!Serial && i < 50) {
+    delay(10);
+    i++;
+  }
+  // First message, we are alive
+  Serial.println("");
+  Serial.println("Init");
 
-  pms.init();
+  // Initialize S8 sensor
+  S8_serial.begin(S8_BAUDRATE);
+  sensor_S8 = new S8_UART(S8_serial);
+
+  //pms5003_serial.begin(S8_BAUDRATE);
+pms5003_serial.init();
+
+
+  // Check if S8 is available
+  sensor_S8->get_firmware_version(sensor.firm_version);
+  int len = strlen(sensor.firm_version);
+  if (len == 0) {
+      Serial.println("SenseAir S8 CO2 sensor not found!");
+      while (1) { delay(1); };
+  }
+
+  // Show basic S8 sensor info
+  Serial.println(">>> SenseAir S8 NDIR CO2 sensor <<<");
+  printf("Firmware version: %s\n", sensor.firm_version);
+  sensor.sensor_id = sensor_S8->get_sensor_ID();
+  Serial.print("Sensor ID: 0x"); printIntToHex(sensor.sensor_id, 4); Serial.println("");
+
+  Serial.println("Setup done!");
+  Serial.flush();
 }
 
-void loop()
-{
-  // read the PM sensor
-  pms.read();
-  if (pms)
-  { // successfull read
-#ifdef ESP8266
-    // print formatted results
-    Serial.printf("PM1.0 %2d, PM2.5 %2d, PM10 %2d [ug/m3]\n",
-                  pms.pm01, pms.pm25, pms.pm10);
 
-    if (pms.has_number_concentration())
-      Serial.printf("N0.3 %4d, N0.5 %3d, N1.0 %2d, N2.5 %2d, N5.0 %2d, N10 %2d [#/100cc]\n",
-                    pms.n0p3, pms.n0p5, pms.n1p0, pms.n2p5, pms.n5p0, pms.n10p0);
+void loop() {
+  
+  //printf("Millis: %lu\n", millis());
 
-    if (pms.has_temperature_humidity() || pms.has_formaldehyde())
-      Serial.printf("%5.1f °C, %5.1f %%rh, %5.2f mg/m3 HCHO\n",
-                    pms.temp, pms.rhum, pms.hcho);
-#else
-    // print the results
+  // Get CO2 measure
+  sensor.co2 = sensor_S8->get_co2();
+  printf("CO2 value = %d ppm\n", sensor.co2);
+
+  //Serial.printf("/*%u*/\n", sensor.co2);   // Format to use with Serial Studio program
+
+  // Compare with PWM output
+  //sensor.pwm_output = sensor_S8->get_PWM_output();
+  //printf("PWM output = %0.0f ppm\n", (sensor.pwm_output / 16383.0) * 2000.0);
+
+  // Wait 5 second for next measure
+  delay(5000);
+
+ // pms.read(data);
+ //   Serial.print("PM 1.0 (ug/m3): ");
+  //  Serial.println(data.PM_AE_UG_1_0);
+
+ //   Serial.print("PM 2.5 (ug/m3): ");
+ //   Serial.println(data.PM_AE_UG_2_5);
+
+ //   Serial.print("PM 10.0 (ug/m3): ");
+ //   Serial.println(data.PM_AE_UG_10_0);
+
+  //  Serial.println();
+
+    pms5003_serial.read();
+
     Serial.print(F("PM1.0 "));
-    Serial.print(pms.pm01);
+    Serial.print(pms5003_serial.pm01);
     Serial.print(F(", "));
     Serial.print(F("PM2.5 "));
-    Serial.print(pms.pm25);
+    Serial.print(pms5003_serial.pm25);
     Serial.print(F(", "));
     Serial.print(F("PM10 "));
-    Serial.print(pms.pm10);
+    Serial.print(pms5003_serial.pm10);
     Serial.println(F(" [ug/m3]"));
-
-    if (pms.has_number_concentration())
-    {
-      Serial.print(F("N0.3 "));
-      Serial.print(pms.n0p3);
-      Serial.print(F(", "));
-      Serial.print(F("N0.5 "));
-      Serial.print(pms.n0p5);
-      Serial.print(F(", "));
-      Serial.print(F("N1.0 "));
-      Serial.print(pms.n1p0);
-      Serial.print(F(", "));
-      Serial.print(F("N2.5 "));
-      Serial.print(pms.n2p5);
-      Serial.print(F(", "));
-      Serial.print(F("N5.0 "));
-      Serial.print(pms.n5p0);
-      Serial.print(F(", "));
-      Serial.print(F("N10 "));
-      Serial.print(pms.n10p0);
-      Serial.println(F(" [#/100cc]"));
-    }
-
-    if (pms.has_temperature_humidity() || pms.has_formaldehyde())
-    {
-      Serial.print(pms.temp, 1);
-      Serial.print(F(" °C"));
-      Serial.print(F(", "));
-      Serial.print(pms.rhum, 1);
-      Serial.print(F(" %rh"));
-      Serial.print(F(", "));
-      Serial.print(pms.hcho, 2);
-      Serial.println(F(" mg/m3 HCHO"));
-    }
-#endif
-  }
-  else
-  { // something went wrong
-    switch (pms.status)
-    {
-    case pms.OK: // should never come here
-      break;     // included to compile without warnings
-    case pms.ERROR_TIMEOUT:
-      Serial.println(F(PMS_ERROR_TIMEOUT));
-      break;
-    case pms.ERROR_MSG_UNKNOWN:
-      Serial.println(F(PMS_ERROR_MSG_UNKNOWN));
-      break;
-    case pms.ERROR_MSG_HEADER:
-      Serial.println(F(PMS_ERROR_MSG_HEADER));
-      break;
-    case pms.ERROR_MSG_BODY:
-      Serial.println(F(PMS_ERROR_MSG_BODY));
-      break;
-    case pms.ERROR_MSG_START:
-      Serial.println(F(PMS_ERROR_MSG_START));
-      break;
-    case pms.ERROR_MSG_LENGTH:
-      Serial.println(F(PMS_ERROR_MSG_LENGTH));
-      break;
-    case pms.ERROR_MSG_CKSUM:
-      Serial.println(F(PMS_ERROR_MSG_CKSUM));
-      break;
-    case pms.ERROR_PMS_TYPE:
-      Serial.println(F(PMS_ERROR_PMS_TYPE));
-      break;
-    }
-  }
-
-  // wait for 10 seconds
-  delay(10000);
+  
 }
