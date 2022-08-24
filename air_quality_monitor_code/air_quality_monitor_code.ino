9471ae8885368e74cc762e3311448491496ae7f4
Brent Vreys <12000698@student.pxl.be>
2022-08-25 00:42:38 +0200

    code dust sensor works

--- air_quality_monitor_code/air_quality_monitor_code.ino
+++ air_quality_monitor_code/air_quality_monitor_code.ino
@@ -5,7 +5,7 @@
 //#include "PMS.h"
 /* BEGIN CONFIGURATION */
 #define DEBUG_BAUDRATE 9600
-#include <PMserial.h>
+//#include <PMserial.h>
 
 #define S8_RX_PIN 22        // Rx pin which the S8 Tx pin is attached to (change if it is needed)
 #define S8_TX_PIN 23         // Tx pin which the S8 Rx pin is attached to (change if it is needed)
@@ -13,17 +13,27 @@ SoftwareSerial S8_serial(S8_RX_PIN, S8_TX_PIN);
 S8_UART *sensor_S8;
 S8_sensor sensor;
 
-#define pms5003_RX_PIN 0        // Rx pin which the S8 Tx pin is attached to (change if it is needed)
-#define pms5003_TX_PIN 2         // Tx pin which the S8 Rx pin is attached to (change if it is needed)
-SerialPM  pms5003_serial(PMS5003, pms5003_RX_PIN, pms5003_TX_PIN);
+#define pms5003_RX_PIN 19       // Rx pin which the S8 Tx pin is attached to (change if it is needed)
+#define pms5003_TX_PIN 18         // Tx pin which the S8 Rx pin is attached to (change if it is needed)
+SoftwareSerial  pmsSerial( pms5003_RX_PIN, pms5003_TX_PIN);
 //PMS pms(pms5003_serial);
 //PMS::DATA data;
 
+struct pms5003data {
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+
+struct pms5003data data;
 
 void setup() {
   
   // Configure serial port, we need it for debug
-  Serial.begin(DEBUG_BAUDRATE);
+  Serial.begin(115200);
 
   // Wait port is open or timeout
   int i = 0;
@@ -40,7 +50,7 @@ void setup() {
   sensor_S8 = new S8_UART(S8_serial);
 
   //pms5003_serial.begin(S8_BAUDRATE);
-pms5003_serial.init();
+pmsSerial.begin(9600);
 
 
   // Check if S8 is available
@@ -77,30 +87,80 @@ void loop() {
   //printf("PWM output = %0.0f ppm\n", (sensor.pwm_output / 16383.0) * 2000.0);
 
   // Wait 5 second for next measure
-  delay(5000);
-
- // pms.read(data);
- //   Serial.print("PM 1.0 (ug/m3): ");
-  //  Serial.println(data.PM_AE_UG_1_0);
-
- //   Serial.print("PM 2.5 (ug/m3): ");
- //   Serial.println(data.PM_AE_UG_2_5);
-
- //   Serial.print("PM 10.0 (ug/m3): ");
- //   Serial.println(data.PM_AE_UG_10_0);
-
-  //  Serial.println();
-
-    pms5003_serial.read();
-
-    Serial.print(F("PM1.0 "));
-    Serial.print(pms5003_serial.pm01);
-    Serial.print(F(", "));
-    Serial.print(F("PM2.5 "));
-    Serial.print(pms5003_serial.pm25);
-    Serial.print(F(", "));
-    Serial.print(F("PM10 "));
-    Serial.print(pms5003_serial.pm10);
-    Serial.println(F(" [ug/m3]"));
   
+  if (readPMSdata(&pmsSerial)) {
+    // reading data was successful!
+    Serial.println();
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (standard)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (environmental)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
+    Serial.println("---------------------------------------");
+    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
+    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
+    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
+    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
+    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
+    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
+    Serial.println("---------------------------------------");
+  }
+
+  //delay(1000);
+  
+}
+
+boolean readPMSdata(Stream *s) {
+  if (! s->available()) {
+    return false;
+  }
+  
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  if (s->peek() != 0x42) {
+    s->read();
+    return false;
+  }
+ 
+  // Now read all 32 bytes
+  if (s->available() < 32) {
+    return false;
+  }
+    
+  uint8_t buffer[32];    
+  uint16_t sum = 0;
+  s->readBytes(buffer, 32);
+ 
+  // get checksum ready
+  for (uint8_t i=0; i<30; i++) {
+    sum += buffer[i];
+  }
+ 
+  /* debugging
+  for (uint8_t i=2; i<32; i++) {
+    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+  }
+  Serial.println();
+  */
+  
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i=0; i<15; i++) {
+    buffer_u16[i] = buffer[2 + i*2 + 1];
+    buffer_u16[i] += (buffer[2 + i*2] << 8);
+  }
+ 
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+ 
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
 }
